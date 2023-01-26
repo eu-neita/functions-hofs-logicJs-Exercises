cd14d01d82e22af7dddf696bc564bf51ee371e8e
neita <neitass.music@gmail.com>
2023-01-26 16:31:11 -0300

    termina totalmente 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,15 +2,7 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   const animals = data.species.find((specie) => specie.name === animal);
-  const cu = [];
-  let tamanho = 0;
-  animals.residents.forEach((element) => (element.age >= age ? cu.push(element.age) : 0));
-  animals.residents.forEach((element) => {
-    if (element.age < 100) {
-      tamanho += 1;
-    }
-  });
-  return cu.length === tamanho;
+  return animals.residents.every((element) => element.age >= age);
 };
 console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
