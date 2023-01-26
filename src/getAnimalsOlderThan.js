f0a078af613807485210cf60933fd08dee636b2f
neita <neitass.music@gmail.com>
2023-01-26 16:19:24 -0300

    termina requisito 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animals = data.species.find((specie) => specie.name === animal);
+  const cu = [];
+  let tamanho = 0;
+  animals.residents.forEach((element) => (element.age >= age ? cu.push(element.age) : 0));
+  animals.residents.forEach((element) => {
+    if (element.age < 100) {
+      tamanho += 1;
+    }
+  });
+  return cu.length === tamanho;
 };
-
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const pu = [];
+  if (pu === []) {
+    return pu;
+  }
+  ids.forEach((id, i) => {
+    pu.push(data.species.find((specie) => specie.id === ids[i]));
+  });
+
+  return pu;
 };
 
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
