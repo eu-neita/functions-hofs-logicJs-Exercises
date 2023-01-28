10542c21c289c2519f1c1512f96f3f9f50c9b3a7
neita <neitass.music@gmail.com>
2023-01-28 01:53:14 -0300

    termina requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === null || animal === undefined) {
+    const u = {};
+    data.species.map((valores) => eval(`u.${valores.name}= ${valores.residents.length}`));
+    return u;
+  }
+  if (animal.sex) {
+    const sexos = [];
+    const todos = data.species.find((animaisinhos) => animaisinhos.name === animal.species);
+    todos.residents.forEach((residentes) => {
+      if (residentes.sex === animal.sex) {
+        sexos.push(residentes);
+      }
+    });
+    return sexos.length;
+  }
+  return data.species.find((animaisinhos) => animaisinhos.name === animal.species).residents.length;
 };
-
+// console.log(countAnimals());
 module.exports = countAnimals;
