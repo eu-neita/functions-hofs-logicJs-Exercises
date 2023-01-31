6c359ab766e8341155d3e5dd555c6110ca730489
neita <neitass.music@gmail.com>
2023-01-31 19:34:22 -0300

    termina requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const veryffyReturn = (nameOrId) => {
+  const dataEm = data.employees.find((per) => per.firstName === nameOrId.name
+  || per.lastName === nameOrId.name || nameOrId.id === per.id);
+  const { firstName, lastName, responsibleFor } = dataEm;
+  const namesAni = dataEm.responsibleFor.map((res, i) => data.species.find((datas) =>
+    datas.id === res).name);
+  const locations = responsibleFor.map((res, i) => data.species.find((datas) =>
+    datas.id === res).location);
+  return {
+    id: dataEm.id,
+    fullName: `${firstName} ${lastName}`,
+    species: namesAni,
+    locations,
+  };
 };
 
+const getEmployeesCoverage = (name) => {
+  if (name === undefined) {
+    return data.employees.map((all) => veryffyReturn(all));
+  }
+  const dataEm = data.employees.find((per) => per.firstName === name.name
+  || per.lastName === name.name || name.id === per.id);
+  if (dataEm === undefined) throw new Error('Informações inválidas');
+  return veryffyReturn(name);
+};
+console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
