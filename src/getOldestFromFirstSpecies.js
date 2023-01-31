d2513f49fc1df22734b2308d2c2c41d7cd5d0042
neita <neitass.music@gmail.com>
2023-01-31 16:39:35 -0300

    termina requisito 10 foramata

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,21 +4,18 @@ const getOldestFromFirstSpecies = (id) => {
   const { employees, species } = data;
   const firstAnimal = employees.find((person) => id === person.id).responsibleFor[0];
   const animalsResidents = species.find((animal) => animal.id === firstAnimal).residents;
-  const oldestAnimal = animalsResidents.sort((a, b) => b.age - a.age)[0];
-  const { name, sex, age } = oldestAnimal;
+  const { name, sex, age } = animalsResidents.sort((a, b) => b.age - a.age)[0];
   return [name, sex, age];
 };
 
 module.exports = getOldestFromFirstSpecies;
 
 // const getOldestFromFirstSpeciesX = (id) => {
-//   const emptyAnimal = { age: 0 };
-//   const { employees, species } = data;
-//   const firstAnimal = employees.find((person) => id === person.id).responsibleFor[0];
-//   const { name, sex, age } = species.find(
-//     (animal) => animal.id === firstAnimal).residents.reduce(
-//       (lhs, rhs) => (lhs.age > rhs.age ? lhs : rhs),
-//       emptyAnimal);
+//   const { name, sex, age } = data.species.find(
+//     (animal) => animal.id === data.employees.find((person) => id === person.id).responsibleFor[0],
+//   ).residents.reduce((lhs, rhs) => (lhs.age > rhs.age ? lhs : rhs),
+//     { age: 0 });
 //   return [name, sex, age];
 // };
-// console.log(getOldestFromFirstSpeciesX('9e7d4524-363c-416a-8759-8aa7e50c0992'))
+// console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'))
+// formatadin sem erro kkkkkk
