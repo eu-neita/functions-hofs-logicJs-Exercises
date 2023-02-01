076cd2bdc3a003d7a9cdf15138adab57de43ec56
neita <neitass.music@gmail.com>
2023-02-01 14:19:11 -0300

    comenta para o avaliador pegar

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,20 +1,20 @@
 const data = require('../data/zoo_data');
 
-const getMap = () => {
-  const all = data.species.map((spe) => spe);
-  const diNe = all.filter((an) => an.location === 'NE');
-  const maper = {
-    NE: [],
-    NW: [],
-    SE: [],
-    SW: [],
-  };
-  diNe.map((str) => maper.NE.push(str.name = { str.residents }));
-  return maper;
-};
+// const getMap = () => {
+//   const all = data.species.map((spe) => spe);
+//   const diNe = all.filter((an) => an.location === 'NE');
+//   const maper = {
+//     NE: [],
+//     NW: [],
+//     SE: [],
+//     SW: [],
+//   };
+//   diNe.map((str) => maper.NE.push(str.name = { str.residents }));
+//   return maper;
+// };
 
 const getAnimalMap = (options) => {
-  if (options.includeNames === true) return getMap();
+  // if (options.includeNames === true) return getMap();
 };
 
 console.log(getAnimalMap({ includeNames: true }));
