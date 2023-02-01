68e28c1d897379fc863dbd762d9576784f7c4a32
neita <neitass.music@gmail.com>
2023-02-01 14:09:19 -0300

    salva requisito 12 e 13

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
+const getMap = () => {
+  const all = data.species.map((spe) => spe);
+  const diNe = all.filter((an) => an.location === 'NE');
+  const maper = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  diNe.map((str) => maper.NE.push(str.name = { str.residents }));
+  return maper;
 };
+
+const getAnimalMap = (options) => {
+  if (options.includeNames === true) return getMap();
+};
+
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,7 +1,7 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
-  it('se o retorno da função sem nenuhm parametro retorna os horários', () => {
+  it('se o retorno da função sem nenuhm parametro retorna os horários da semana', () => {
     const actual = getOpeningHours();
     const expected = {
       Tuesday: { open: 8, close: 6 },
@@ -14,7 +14,18 @@ describe('Testes da função getOpeningHours', () => {
     };
     expect(actual).toEqual(expected);
   });
-  it('testando o erro', () => {
-    expect(getOpeningHours('AM:08-6')).toThrowAnyError();
+  it('testando se apasado os parametro corretamente o retorno é o esperado The zoo is closed', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
   });
+  it('testando se apasado os parametro corretamente o retorno é o esperado The zoo is open', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('testando se apasado os parametro corretamente o retorno é o esperado The zoo is closed com o horario diferente', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  // it('testando erro', () => {
+  //   const actual = getOpeningHours('Thu', '09:00-AM');
+  //   const expected = new Error('The day must be valid. Example: Monday');
+  //   expect(actual).toThrow(expected);
+  // });
 });
